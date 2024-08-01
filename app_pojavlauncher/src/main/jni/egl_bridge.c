f53a05d768bfd52817155f8cce6eb5b8fa635b11
SolDev69 <michaelcraft1104@gmail.com>
2024-08-01 11:10:39 -0400

    try fix virgl

--- app_pojavlauncher/src/main/jni/egl_bridge.c
+++ app_pojavlauncher/src/main/jni/egl_bridge.c
@@ -241,7 +241,7 @@ int pojavInitOpenGL() {
 
     // NOTE: Override for now.
     const char *renderer = getenv("POJAV_RENDERER");
-    if (strcmp(renderer, "virgl") == 0 || strcmp(renderer, "virgl_a") == 0) {
+    if (strncmp("virgl", renderer, 15) == 0) {
         solcraft_environ->config_renderer = RENDERER_VIRGL;
         setenv("GALLIUM_DRIVER","virpipe",1);
         if(!checkAdrenoGraphics())
